b5adff1be9a5984264a982fd273e87e923ff4673
grefano <gabrielnieed@gmail.com>
2026-03-31 22:02:11 -0300

    created build.sh

--- kernel.c
+++ kernel.c
@@ -63,7 +63,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BROWN);
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
 	
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -111,5 +111,5 @@ void kernel_main(void)
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, editing World!\n");
+	terminal_writestring("oi resenhudos, rs teste\n");
 }
