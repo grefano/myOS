a1c5babfcfb8723e67273f060a85357044d99ee1
grefano <gabrielnieed@gmail.com>
2026-03-31 22:02:11 -0300

    created build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -ex
+
+~/opt/cross/bin/i686-elf-as boot.s -o boot.o
+~/opt/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+~/opt/cross/bin/i686-elf-gcc -T linker.ld -o myos -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+rm -r isodir
+mkdir -p isodir/boot/grub
+cp myos isodir/boot/myos
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
