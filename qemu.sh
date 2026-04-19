9efd4c847c22b0ba542b5d75f10dc8b3f75833a6
grefano <gabrielnieed@gmail.com>
2026-04-19 10:44:49 -0300

    fix memset and memcpy

--- qemu.sh
+++ qemu.sh
@@ -2,5 +2,4 @@ set -e
 
 qemu-system-i386 -cdrom myos.iso -s -S -d int,cpu_reset -no-shutdown -no-reboot -D log.txt &
 sleep 1
-gdb isodir/boot/myos -x debug.gdb \
-  -ex "target remote :1234"
+gdb isodir/boot/myos -x debug.gdb
